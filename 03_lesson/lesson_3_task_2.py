c689cbca6c814e9b89db2ef5ae18bfec234159eb
Iratest472 <kurs30895@gmail.com>
2025-03-09 11:48:41 +0300

    готово2

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -3,7 +3,9 @@ from smartphone import Smartphone
 catalog = [
     Smartphone("Sony", "AX", "+79001122334"),
     Smartphone("Samsung", "Note10", "+79001122333"),
-    Smartphone("Nokia", "i900", "+79005522333")
+    Smartphone("Nokia", "i900", "+79005522333"),
+    Smartphone("Samsung", "x224", "+79005555555"),
+    Smartphone("Nokia", "i700", "+79005522222")
 ]
 
 for smartphone in catalog:
